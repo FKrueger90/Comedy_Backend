e887a9703f9617625d542c27daed604c9448e031
FKrueger90 <florian.krueger@protonmail.com>
2022-08-12 14:11:03 -0400

    inital

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-
-const mongoose = require("mongoose")
+/** @member {Object} */
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect('mongodb://admin:cern1954@97.107.141.97:27017', {
+        const connection = await mongoose.connect('mongodb://admin:cern1954@127.0.0.1:27017', {
             useNewUrlParser: true,
-                useUnifiedTopology: true,
-                dbName: 'ComedyRadar',
+            useUnifiedTopology: true,
+            dbName: 'ComedyRadar',
         })
         console.log(`Mongo DB connected: ${connection.connection.host}`.cyan.underline)
         //const db = mongoose.connection
